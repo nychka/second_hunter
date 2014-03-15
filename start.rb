1c7996d79e0d21f8bf6b21ac63ea7b9a90950051
nychos <nychka08@yandex.ru>
2014-03-15 02:10:10 +0200

    added geocode helper

--- start.rb
+++ start.rb
@@ -1,18 +1,47 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/activerecord'
 require 'sinatra/json'
+#require 'sinatra/async'
+#require "em-http-request"
+require 'net/http'
 require_relative 'db/database.rb'
 
 class SecondHunter < Sinatra::Base
+  #register Sinatra::Async
   helpers Sinatra::JSON
   set :server, :thin
   set :sessions, true
   set :logging, true
+  set :threaded, true
     
   configure do
     set :threaded, true
     set :root, "app/"
     set :public_folder, Proc.new { File.join(root, "assets") }
+  end
+  helpers do
+    def geocode(address)
+      raise ArgumentError, "Address is NOT passed!" if address.nil? or address.is_a?(String) == false
+      address ="#{address}+,Івано-Франківськ,+Україна".gsub!(/\s/, "+")
+      url="maps.googleapis.com"
+      path = "/maps/api/geocode/json?address=#{address}s&sensor=false"# &key=AIzaSyDQWuApUanpte7w4Enl4WDKrIQ0m5cYf7E"
+      p "Waiting for response from google maps ..."
+      response = Net::HTTP.get_response(url, path)
+      if response.code.to_i == 200
+        result = JSON.parse(response.body)
+        status = result["status"]
+        if status == "OK"
+          #витягуємо широту та довготу
+          location = result["results"][0]["geometry"]["location"]
+          p location
+          location
+        else
+          raise StandardError, "status is not ok: #{status}"
+        end
+      else
+        raise StandardError, "bad request, code #{response.code}"
+      end
+    end
   end
   get '/' do
     @title = "Second Hunter - for real clothes hunters!"
@@ -26,9 +55,12 @@ class SecondHunter < Sinatra::Base
     @shop = Shop.all
     json @shop
   end
-  get '/code' do
-    address="1600+Amphitheatre+Parkway,+Mountain+View,+CA"
-    url="http://maps.googleapis.com/maps/api/geocode/json?address=#{address}s&sensor=false"
+  get '/delay/:n' do |n|
+    EM.add_timer(n.to_i) { body { "delayed for #{n} seconds" } }
+  end
+  get '/geocode/:address' do
+    location = geocode(params[:address])
+    body{location.to_s}
   end
 end
 
