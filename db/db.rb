980ea7a3605efd9c2a72cf6eafa8aa7fdf8c15a3
nychos <nychka08@yandex.ru>
2014-03-28 00:48:35 +0200

    changed mongoid env to production

--- db/db.rb
+++ db/db.rb
@@ -1,6 +1,6 @@
 require "mongoid"
 
-Mongoid.load!("config/mongoid.yml", :development)
+Mongoid.load!("config/mongoid.yml", ENV['RACK_ENV'])
 Mongoid.raise_not_found_error = false
 class User
   include Mongoid::Document
